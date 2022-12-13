e49db570c42234af9aff6b58805a334b554c30be
Kai <krikhye@gmail.com>
2022-12-12 20:43:36 -0600

    feat(hello): greet world if no name is passed in

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,8 @@ package main
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Kai")
-	want := "Hello, Kai"
+	t.Run("says hello to people", func(t *testing.T) {
+		got := Hello("Kai")
+		want := "Hello, Kai"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
