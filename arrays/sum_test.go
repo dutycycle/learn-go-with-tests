b677442c2b7e21ccb7c7cb46af058427bb4d118b
Kai <krikhye@gmail.com>
2022-12-18 15:22:16 -0600

    refactor(array): extract int slice equals to helper function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -19,28 +19,26 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	checkSums(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T) {
 	t.Run("make the sums of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 10})
 		want := []int{5, 19}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
+}
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
